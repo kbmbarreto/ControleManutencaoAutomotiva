39b846c891626b04c946eed5cb7bf9bb5e3acca0
Kleber Barreto de Macedo <kleber.barreto@msn.com>
2021-07-13 22:59:17 -0300

    Debug

--- Model/dbstring.cs
+++ Model/dbstring.cs
@@ -8,7 +8,7 @@ namespace Model
 {
     public class Dbstring
     {
-        public static string Cnx = @"Data Source=KBMNITRO\localhost;Initial Catalog=ManutencaoVeicular;
+        public static string Cnx = @"Data Source=KBMNITRO\KBMNITRO;Initial Catalog=ManutencaoVeicular;
 Persist Security Info=False;User ID=sa;Password=Dev2020@;MultipleActiveResultSets=False;Encrypt=False;
 TrustServerCertificate=False;Connection Timeout=30;";
     }
